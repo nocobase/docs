52b71beb2ae3da74d6b7edb19e838f8f0065d68f
chenos <chenos@users.noreply.github.com>
2023-12-04 07:29:18 +0000

    deploy: 3c122973aef2039cfbe4e8a265e8279a989b3801

---
+++ 1494.1d6fa20a.async.js
@@ -0,0 +1,18 @@
+(self.webpackChunknocobase_docs=self.webpackChunknocobase_docs||[]).push([[1494],{64217:function(h,i,t){"use strict";t.d(i,{Z:function(){return g}});var l=t(1413),c=`accept acceptCharset accessKey action allowFullScreen allowTransparency
+    alt async autoComplete autoFocus autoPlay capture cellPadding cellSpacing challenge
+    charSet checked classID className colSpan cols content contentEditable contextMenu
+    controls coords crossOrigin data dateTime default defer dir disabled download draggable
+    encType form formAction formEncType formMethod formNoValidate formTarget frameBorder
+    headers height hidden high href hrefLang htmlFor httpEquiv icon id inputMode integrity
+    is keyParams keyType kind label lang list loop low manifest marginHeight marginWidth max maxLength media
+    mediaGroup method min minLength multiple muted name noValidate nonce open
+    optimum pattern placeholder poster preload radioGroup readOnly rel required
+    reversed role rowSpan rows sandbox scope scoped scrolling seamless selected
+    shape size sizes span spellCheck src srcDoc srcLang srcSet start step style
+    summary tabIndex target title type useMap value width wmode wrap`,d=`onCopy onCut onPaste onCompositionEnd onCompositionStart onCompositionUpdate onKeyDown
+    onKeyPress onKeyUp onFocus onBlur onChange onInput onSubmit onClick onContextMenu onDoubleClick
+    onDrag onDragEnd onDragEnter onDragExit onDragLeave onDragOver onDragStart onDrop onMouseDown
+    onMouseEnter onMouseLeave onMouseMove onMouseOut onMouseOver onMouseUp onSelect onTouchCancel
+    onTouchEnd onTouchMove onTouchStart onScroll onWheel onAbort onCanPlay onCanPlayThrough
+    onDurationChange onEmptied onEncrypted onEnded onError onLoadedData onLoadedMetadata
+    onLoadStart onPause onPlay onPlaying onProgress onRateChange onSeeked onSeeking onStalled onSuspend onTimeUpdate onVolumeChange onWaiting onLoad onError`,u="".concat(c," ").concat(d).split(/[\s\n]+/),p="aria-",m="data-";function r(a,o){return a.indexOf(o)===0}function g(a){var o=arguments.length>1&&arguments[1]!==void 0?arguments[1]:!1,e;o===!1?e={aria:!0,data:!0,attr:!0}:o===!0?e={aria:!0}:e=(0,l.Z)({},o);var s={};return Object.keys(a).forEach(function(n){(e.aria&&(n==="role"||r(n,p))||e.data&&r(n,m)||e.attr&&u.includes(n))&&(s[n]=a[n])}),s}},52361:function(){},94616:function(){}}]);
