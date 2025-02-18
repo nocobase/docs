699ecc306abf1660c43cf6f4e69f8f2a820028b9
mytharcher <mytharcher@gmail.com>
2025-02-18 15:50:27 +0800

    fix: cluster mode hint

--- .dumi/theme/builtins/PluginInfo.tsx
+++ .dumi/theme/builtins/PluginInfo.tsx
@@ -1,11 +1,34 @@
 import { Link, useSiteData } from 'dumi';
 import React from 'react';
 
+function PluginLink(props) {
+  const { name, link } = props;
+  return <Link to={link ?? `/handbook/${name}`}>plugin-{name}</Link>;
+}
+
+function PluginList({ plugins, name, link }) {
+  return plugins ? plugins.split(',').map((item, i) => (
+    <>
+      <PluginLink name={item} />
+      {i < plugins.split(',').length - 1 ? ', ' : ''}
+    </>
+  )) : <PluginLink name={name} link={link} />;
+}
+
 const PluginInfo = (props) => {
-  const { name, link, commercial, licenseBundled } = props;
+  const { name, link, plugins, commercial, licenseBundled } = props;
   const { themeConfig } = useSiteData();
   const zhCN = themeConfig.lang === 'zh-CN';
   const jaJP = themeConfig.lang === 'ja-JP';
+  const bundleTexts = licenseBundled === 'enterprise' ? {
+    'zh-CN': '企业版',
+    'ja-JP': '企业版',
+    'en-US': 'commercial edition',
+  } : {
+    'zh-CN': '商业版',
+    'ja-JP': '商业版',
+    'en-US': 'commercial edition',
+  }
 
   return (
     <div>
@@ -19,15 +42,15 @@ const PluginInfo = (props) => {
             <section>
               {zhCN ? (
                 <p>
-                  该功能由商业插件 <Link to={link ?? `/handbook/${name}`}>plugin-{name}</Link> 提供，包含在 <a target="_blank" href="https://www.nocobase.com/cn/commercial">商业版本</a> 中，无需单独购买。
+                  该功能由商业插件 <PluginList name={name} link={link} plugins={plugins} /> 提供，包含在 <a target="_blank" href="https://www.nocobase.com/cn/commercial">{bundleTexts[themeConfig.lang]}</a> 中，无需单独购买。
                 </p>
               ) : jaJP ? (
                 <p>
-                  この機能は商用プラグイン <Link to={link ?? `/handbook/${name}`}>plugin-{name}</Link> によって提供されます，包含在 <a target="_blank" href="https://www.nocobase.com/jp/commercial">商业版本</a> 中，无需单独购买。
+                  この機能は商用プラグイン <PluginList name={name} link={link} plugins={plugins} /> によって提供されます，包含在 <a target="_blank" href="https://www.nocobase.com/jp/commercial">{bundleTexts[themeConfig.lang]}</a> 中，无需单独购买。
                 </p>
               ) : (
                 <p>
-                  This feature is provided by the commercial plugin <Link to={link ?? `/handbook/${name}`}>plugin-{name}</Link>, included in the <a target="_blank" href="https://www.nocobase.com/en/commercial">commercial edition</a>, with no additional purchase required.
+                  This feature is provided by the commercial plugin <PluginList name={name} link={link} plugins={plugins} />, included in the <a target="_blank" href="https://www.nocobase.com/en/commercial">{bundleTexts[themeConfig.lang]}</a>, with no additional purchase required.
                 </p>
               )}
             </section>
@@ -35,15 +58,15 @@ const PluginInfo = (props) => {
             <section>
               {zhCN ? (
                 <p>
-                  该功能由{commercial ? '商业' : ''}插件 <Link to={link ?? `/handbook/${name}`}>plugin-{name}</Link> 提供{commercial && <>，请查看 <a target="_blank" href="https://www.nocobase.com/cn/plugins">商业授权</a> 了解详情</>}.
+                  该功能由{commercial ? '商业' : ''}插件 <PluginList name={name} link={link} plugins={plugins} /> 提供{commercial && <>，请查看 <a target="_blank" href="https://www.nocobase.com/cn/plugins">商业授权</a> 了解详情</>}.
                 </p>
               ) : jaJP ? (
                 <p>
-                  この機能は{commercial ? '商用' : ''}プラグイン <Link to={link ?? `/handbook/${name}`}>plugin-{name}</Link> によって提供されます。{commercial && <> 詳細については、<a target="_blank" href="https://www.nocobase.com/jp/plugins">商用ライセンス</a> をご覧ください。</>}
+                  この機能は{commercial ? '商用' : ''}プラグイン <PluginList name={name} link={link} plugins={plugins} /> によって提供されます。{commercial && <> 詳細については、<a target="_blank" href="https://www.nocobase.com/jp/plugins">商用ライセンス</a> をご覧ください。</>}
                 </p>
               ) : (
                 <p>
-                  This feature is provided by the {commercial ? 'commercial ' : ''}plugin <Link to={link ?? `/handbook/${name}`}>plugin-{name}</Link>. {commercial && <><br /> Please refer to the <a target="_blank" href="https://www.nocobase.com/en/plugins">commercial license</a> for details.</>}
+                  This feature is provided by the {commercial ? 'commercial ' : ''}plugin <PluginList name={name} link={link} plugins={plugins} />. {commercial && <><br /> Please refer to the <a target="_blank" href="https://www.nocobase.com/en/plugins">commercial license</a> for details.</>}
                 </p>
               )}
             </section>
