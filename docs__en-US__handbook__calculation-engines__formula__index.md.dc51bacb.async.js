953677bcc95fb514e8682fa390f51f5a4dcfec65
Albert-mah <Albert-mah@users.noreply.github.com>
2025-04-08 11:12:29 +0000

    deploy: ca60fe61c9b8324245e984f20ce884d0cc39c305

---
+++ docs__en-US__handbook__calculation-engines__formula__index.md.dc51bacb.async.js
@@ -0,0 +1,5 @@
+"use strict";(self.webpackChunknocobase_docs=self.webpackChunknocobase_docs||[]).push([[86081],{10930:function(u,s,_){_.r(s);var i=_(71100),m=_(35532),r=_(37402),l=_(78274),E=_(75208),a=_(92716),h=_(66313),c=_(48640),M=_(35206),d=_(75553),P=_(56266),D=_(72333),O=_(41118),x=_(39297),C=_(68526),I=_(5019),o=_(96707),T=_(67294),n=_(85893);function e(){var t=(0,o.eL)(),v=t.texts;return(0,n.jsx)(o.dY,{children:(0,n.jsxs)(n.Fragment,{children:[(0,n.jsxs)("div",{className:"markdown",children:[(0,n.jsxs)("h1",{id:"formulajs-functions",children:[(0,n.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#formulajs-functions",children:(0,n.jsx)("span",{className:"icon icon-link"})}),"Formula.js Functions"]}),(0,n.jsx)("style",{dangerouslySetInnerHTML:{__html:`
+article{
+  max-width: 90% !important;
+}
+`}})]}),(0,n.jsx)(d.Z,{})]})})}s.default=e}}]);
